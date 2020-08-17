f35765fe278b5e23ef8e98d8bb9df95755c832e1
scottdao <13466484073@139.com>
2020-08-17 21:52:33 +0800

    add file

--- files/utils/useFetch.js
+++ files/utils/useFetch.js
@@ -1,45 +1,55 @@
-class UseFetch{
-    constructor() {
-        this.BASE_URL = null;
+class UseFetch {
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
         this.default_head = {};
-        this.request = function (compose_url, bodyHead) {
-            return new Promise((resovle,reject)=>{
-                    fetch(compose_url, {
-                        ...bodyHead
-                    }).then(respose=>{
-                        if (response.status >= 200 && response.status < 300) {
-                                return Promise.resolve(response);
-                        }
-                        return Promise.reject(new Error(response.statusText));
-                    })
-                    .then(response => response.json())
-                    .then(result => {
-                        resolve(result)
-                    })
-                    .catch(error => {
-                        reject(error)
-                    });
-            })
-        }
     }
-    get(){}
-    post(url, bodyHeaders){
+    request(compose_url, bodyHead) {
+        return new Promise((resolve, reject) => {
+            fetch(compose_url, {
+                    ...bodyHead
+                }).then(response => {
+                    if (response.status >= 200 && response.status < 300) {
+                        return Promise.resolve(response);
+                    }
+                    return Promise.reject(new Error(response.statusText));
+                })
+                .then(response => response.json())
+                .then(result => {
+                    resolve(result)
+                })
+                .catch(error => {
+                    reject(error)
+                });
+        })
+    }
+    get(url) {
         let compose_url = url;
-        if(this.BASE_URL){
-            compose_url = this.BASE_URL+url;
+        if (this.BASE_URL) {
+            compose_url = this.BASE_URL + url;
         }
-        let bodyHead = {
-                ...this.default_head,
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        const bodyHead = {
+            ...this.default_head,
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
         }
-        if(bodyHeaders.type === 'json'){
+        return this.request(url, bodyHeaders)
+    }
+    post(url, bodyHeaders) {
+        let compose_url = url;
+        if (this.BASE_URL) {
+            compose_url = this.BASE_URL + url;
+        }
+        const bodyHead = {
+            ...this.default_head,
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        }
+        if (bodyHeaders.type === 'json') {
             bodyHead.headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
             bodyHead.body = JSON.stringify(bodyHeaders.body)
-        }else if(bodyHeaders === 'formData'){
+        } else {
             bodyHead.body = bodyHeaders.body;
         }
-        return  this.request(url, bodyHeaders)
+        return this.request(url, bodyHeaders)
     }
-    
+
 }
 const useFetch = new UseFetch();
